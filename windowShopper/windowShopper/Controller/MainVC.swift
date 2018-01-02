528398ebb84084df8c5e6a33293028bbcbd318b4
Duong Dang Hoang <alex@cpe-101-190-50-216.nb03.nsw.asp.telstra.net>
2018-01-02 16:23:16 +1100

    Custom txt field

--- windowShopper/windowShopper/Controller/MainVC.swift
+++ windowShopper/windowShopper/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  windowShopper
 //
 //  Created by Duong Dang Hoang on 2/1/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
