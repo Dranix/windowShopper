a11db82f531258f3296ceb5d5ddb347f1f1d5871
Duong Dang Hoang <alex@cpe-101-190-50-216.nb03.nsw.asp.telstra.net>
2018-01-02 15:25:14 +1100

    Initialization

---
+++ windowShopper/windowShopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  windowShopper
+//
+//  Created by Duong Dang Hoang on 2/1/18.
+//  Copyright © 2018 Duong Dang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
